5bbd7ca0a1437e3b417ca39da9f4a6e7555cdcb4
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-04-23 17:43:30 -0500

    importPlugins2.0

--- src/pages/informacion.ts
+++ src/pages/informacion.ts
@@ -2,3 +2,9 @@
 /*de crearon las paginas "guardados","mapa"y "tabs" , se configuro el "app.component.ts" y "app.module.ts"
 se agrego una variable de color en "variables.scss", ademas, se creo la carpeta "models" y dentro se creo el
 el archivo "scan-data.model.ts" creando un constructor y dos variables dentro del archivo*/
+
+/*commit importPlugins
+en este commit se implemento el plugin de cordova "Barcode Scanner" para implementar es escaneo de un codigo
+qr con la camara del celular, se realizaron varios cambios incluyendo en la carpeta "app.module.ts" (revisar 
+bien cual fue el cambio), se implemento tambien el mostrar aplicaciones en pantalla con "Toast" implementando 
+desde ionicframework*/
